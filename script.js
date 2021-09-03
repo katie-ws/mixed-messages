26077cc3b754e976d0695714baa7994c00c11207
kws <89932020+katie-ws@users.noreply.github.com>
2021-09-03 16:18:45 +0100

    Add destination

---
+++ script.js
@@ -0,0 +1,3 @@
+let destination = ['Hawaii', 'Denmark', 'Maldives', 'Sri Lanka', 'Japan', 'South Africa', 'Italy', 'Hungary', 'Croatia', 'USA', 'Panama', 'Argentina', 'Costa Rica', 'New Zealand', 'Thailand', 'Syria', 'Myanmar', 'Colombia', 'China', 'Antartica'];
+
+let randomDestination = destination[Math.floor(Math.random()*destination.length)];
